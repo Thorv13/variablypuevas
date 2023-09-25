dfa13c02d7fb44773277fd67b7c947e6ef3ed382
Thorv13 <joan_jmss@hotmail.com>
2023-09-24 22:35:06 -0300

    html desarrollado

--- propio/urls.py
+++ propio/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from appCalculo import views as calculo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('calculo/', calculo.vistap),
 ]
